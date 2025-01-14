1dd2c4dcefdbb94f7f43f38946f4d4c13c0e48b3
Ostap Sulyk <ostapsulyk@gmail.com>
2025-01-14 07:28:44 -0500

    Small refactoring

--- main.go
+++ main.go
@@ -6,13 +6,5 @@ import (
 )
 
 func main() {
-	year2015Days := []utils.Day{
-		year2015.Day01{},
-		year2015.Day02{},
-		year2015.Day03{},
-		year2015.Day04{},
-		year2015.Day05{},
-	}
-
-	utils.PrintDays(2015, year2015Days)
+	utils.PrintDays(2015, year2015.AllDays())
 }
---
+++ year2015/days.go
@@ -0,0 +1,13 @@
+package year2015
+
+import "github.com/ostap-sulyk/advent-of-code/utils"
+
+func AllDays() []utils.Day {
+	return []utils.Day{
+		// Day01{},
+		// Day02{},
+		// Day03{},
+		// Day04{},
+		Day05{},
+	}
+}
